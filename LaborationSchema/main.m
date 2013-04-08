73131d3377101d194eda0aa76cd53fd29fd0cf28
Kristoffer Bergkvist <kristofferbergkvist@Kristoffers-MacBook-Pro.local>
2013-04-08 09:52:55 +0200

    Initial Commit

---
+++ LaborationSchema/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LaborationSchema
+//
+//  Created by Kristoffer Bergkvist on 2013-04-08.
+//  Copyright (c) 2013 Kristoffer Bergkvist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
