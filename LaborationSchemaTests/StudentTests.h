0dd9eb07a766e682f8cbba99fa7d652106cd4e42
Kristoffer Bergkvist <Kristofferbergkvist1993@hotmail.se>
2013-04-18 10:32:31 +0200

    Adding Tests

---
+++ LaborationSchemaTests/StudentTests.h
@@ -0,0 +1,13 @@
+//
+//  StudentTests.h
+//  LaborationSchema
+//
+//  Created by Kristoffer Bergkvist on 2013-04-18.
+//  Copyright (c) 2013 Kristoffer Bergkvist. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface StudentTests : SenTestCase
+
+@end
---
+++ LaborationSchemaTests/StudentTests.m
@@ -0,0 +1,13 @@
+//
+//  StudentTests.m
+//  LaborationSchema
+//
+//  Created by Kristoffer Bergkvist on 2013-04-18.
+//  Copyright (c) 2013 Kristoffer Bergkvist. All rights reserved.
+//
+
+#import "StudentTests.h"
+
+@implementation StudentTests
+
+@end
